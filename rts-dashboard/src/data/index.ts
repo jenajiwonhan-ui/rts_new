ed557e2254396651bcaa189f466f50cf8138c40a
jenajiwonhan-ui <jena.jiwon.han@gmail.com>
2026-03-25 14:36:27 +0900

    수파베이스 db 연결 오류 수정

--- rts-dashboard/src/data/index.ts
+++
@@ -1,8 +0,0 @@
-import rawDataJson from './rawData.json';
-import { RawData } from '../types';
-
-const D = rawDataJson as unknown as RawData;
-export default D;
-
-export const YM = D.filters.ym_list;
-export const WM = D.week_mondays;
